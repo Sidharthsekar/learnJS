3a69374d633a4a223be620a471ae579877ff95af
Sidharth C <sidharth.sekar@gmail.com>
2026-01-10 18:48:43 +0530

    variables

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+let strings = 'honey';
+let numbers = 234;
+let isIt = true;
+let nullValue = null;
+let undef = undefined;
+
+
+console.log(typeof strings)
+console.log(typeof numbers)
+console.log(typeof isIt)
+console.log(typeof nullValue)  //this return type as object instead of null
+console.log(typeof undef);
+
+
+console.log(notdeclaredVariable)  //reference error if not declared
+console.log(typeof notdeclaredVariable) //type is undefined
+
